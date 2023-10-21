28f721badc4670770237207c6f5f2e81f5eb79ec
OofQqooF123 <101352535+OofQqooF123@users.noreply.github.com>
2023-10-21 18:09:17 +0800

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,25 @@
+const audio = document.getElementById("audio");
+let isPlaying = false;
+let isFirstMouseUp = true;
+
+function saveAudioTime() {
+    localStorage.setItem("audioTime", audio.currentTime);
+}
+
+const storedTime = localStorage.getItem("audioTime");
+if (storedTime !== null) {
+    audio.currentTime = parseFloat(storedTime);
+}
+
+
+window.addEventListener("beforeunload", function () {
+    saveAudioTime();
+});
+
+document.addEventListener("mousemove", function () {
+    if (isFirstMouseUp) {
+        audio.play();
+        isFirstMouseUp = false;
+     }
+});
+
